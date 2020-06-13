9008c8f59f18095d8bea57556bc5147b8f30355a
Martin Emil <mar10martinemil@gmail.com>
2020-06-13 03:09:43 +0200

    Modified base Drawable

---
+++ include/tools/drawable.h
@@ -0,0 +1,35 @@
+#ifndef _DRAWABLE_H_
+#define _DRAWABLE_H_
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include "point.h"
+#include "color.h"
+
+class Drawable
+{
+	protected:
+		bool saved;
+		bool filled;
+		Point start_point;
+		Color shape_color;
+	
+	public:
+		bool started;
+
+		Drawable() {}
+		// Drawable(Point start, Point end) {}
+		~Drawable() {}
+	
+		virtual void draw() = 0;
+		virtual void save() = 0;
+		virtual void set_start(Point start, Color color, bool fill) = 0;
+		virtual void change(Point end, Color color,bool fill) = 0;
+		virtual Drawable* make_new() = 0;
+};
+
+#endif
