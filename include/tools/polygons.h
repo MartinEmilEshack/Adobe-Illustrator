8c7801cd3dffeef364f3aac331102efeffe4e96d
Martin Emil <mar10martinemil@gmail.com>
2020-06-13 03:06:55 +0200

    Added Rectangle drawable

--- include/tools/polygons.h
+++ include/tools/polygons.h
@@ -2,32 +2,49 @@
 #define _POLYGONS_H_
 
 #ifdef __APPLE__
-#include<GLUT/glut.h>
+#include <GLUT/glut.h>
 #else
-#include<GL/glut.h>
+#include <GL/glut.h>
 #endif
 
-#include<stdarg.h>
-#include<math.h>
+#include <stdarg.h>
+#include <math.h>
 // #include<iostream>
 
 // using namespace std;
 
-#include"point.h"
+#include "point.h"
+#include "color.h"
+#include "drawable.h"
 
-#define draw_polygon(...) draw_polygon_(false, __VA_ARGS__,Point::getFinal())
-#define draw_polygon_solid(...) draw_polygon_(true, __VA_ARGS__,Point::getFinal())
+class Rectangle : public Drawable
+{
+	private:
+		Point end_point;
+
+	public:
+		Rectangle();
+
+		void draw();
+		void save();
+		void set_start(Point start, Color color, bool fill);
+		void change(Point end, Color color, bool fill);
+		Drawable *make_new();
+};
+
+#define draw_polygon(...) draw_polygon_(false, __VA_ARGS__, Point::getFinal())
+#define draw_polygon_solid(...) draw_polygon_(true, __VA_ARGS__, Point::getFinal())
 
 void draw_polygon_(bool isSolid, Point n, ...);
 
-#define draw_equilateral(x,y,size,sideNum) draw_equilateral_(x,y,size,sideNum,0,false)
-#define draw_equilateral_solid(x,y,size,sideNum) draw_equilateral_(x,y,size,sideNum,0,true)
+#define draw_equilateral(x, y, size, sideNum) draw_equilateral_(x, y, size, sideNum, 0, false)
+#define draw_equilateral_solid(x, y, size, sideNum) draw_equilateral_(x, y, size, sideNum, 0, true)
 
-#define draw_equilateral_rotate(x,y,size,sideNum,degree) draw_equilateral_(x,y,size,sideNum,degree,false)
-#define draw_equilateral_solid_rotate(x,y,size,sideNum,degree) draw_equilateral_(x,y,size,sideNum,degree,true)
+#define draw_equilateral_rotate(x, y, size, sideNum, degree) draw_equilateral_(x, y, size, sideNum, degree, false)
+#define draw_equilateral_solid_rotate(x, y, size, sideNum, degree) draw_equilateral_(x, y, size, sideNum, degree, true)
 
-void draw_equilateral_(float x,float y,float size,int sideNum,int degree,bool isSolid);
+void draw_equilateral_(float x, float y, float size, int sideNum, int degree, bool isSolid);
 
-Point xy(double x,double y);
+Point xy(double x, double y);
 
 #endif
--- src/tools/polygons.cpp
+++ src/tools/polygons.cpp
@@ -1,47 +1,95 @@
-#include"polygons.h"
+#include "polygons.h"
+
+Rectangle::Rectangle()
+{
+	saved = false;
+	started = false;
+}
+
+void Rectangle::draw()
+{
+	shape_color.color();
+	if (filled)
+		glRecti(start_point.x, start_point.y, end_point.x, end_point.y);
+	else
+	{
+		glBegin(GL_LINE_LOOP);
+		{
+			glVertex2i(start_point.x, start_point.y);
+			glVertex2i(end_point.x, start_point.y);
+			glVertex2i(end_point.x, end_point.y);
+			glVertex2i(start_point.x, end_point.y);
+		}
+		glEnd();
+	}
+}
+
+void Rectangle::save()
+{
+	saved = true;
+}
+
+void Rectangle::set_start(Point start, Color color, bool fill)
+{
+	filled = fill;
+	started = true;
+	shape_color = color;
+	start_point = start;
+}
+
+void Rectangle::change(Point end, Color color, bool fill)
+{
+	if (!saved & started)
+	{
+		filled = fill;
+		shape_color = color;
+		end_point = end;
+	}
+}
+
+Drawable *Rectangle::make_new()
+{
+	return new Rectangle();
+}
 
 void draw_polygon_(bool isSolid, Point vertex, ...)
 {
-    if(isSolid) glBegin(GL_POLYGON);
-    else glBegin(GL_LINE_LOOP);
-    {  
-        va_list ap;
-        va_start(ap,vertex);
-        {
-            while (!vertex.invalid)
-            {
-                glVertex2f(vertex.x,vertex.y);
-
-                // cout << "X=" << vertex.x << " Y=" << vertex.y <<"\n";
-                vertex = *va_arg(ap,Point*);
-            }
-
-        } va_end(ap);
-	} glEnd();
+	glBegin(isSolid ? GL_POLYGON : GL_LINE_LOOP);
+	{
+		va_list ap;
+		va_start(ap, vertex);
+		{
+			while (!vertex.invalid)
+			{
+				glVertex2f(vertex.x, vertex.y);
+				// cout << "X=" << vertex.x << " Y=" << vertex.y <<"\n";
+				vertex = *va_arg(ap, Point *);
+			}
+		}
+		va_end(ap);
+	}
+	glEnd();
 }
 
-void draw_equilateral_(float x,float y,float size,int sideNum,int degree,bool isSolid){
-
-    float degreeR = degree*3.1415926f/180.0f;
-
-    if (isSolid) glBegin(GL_POLYGON);
-    else glBegin(GL_LINE_LOOP); 
-    {
-        for (int i = 0; i < sideNum; i++)
-        {
-            float theta = degreeR + 2.0f*3.1415926f*i/sideNum;
-            float xl = size*(cosf(theta));
-            float yl = size*(sinf(theta));
-
-            glVertex2f(x+xl,y+yl);
-            // glColor3f(0.0,1.0,1.0);   
-        }
-    } glEnd();
-
-}
-
-Point xy(double x,double y)
+void draw_equilateral_(float x, float y, float size, int sideNum, int degree, bool isSolid)
 {
-    Point v(x,y);
-    return v;
+	float degreeR = degree * 3.1415926f / 180.0f;
+	glBegin(isSolid ? GL_POLYGON : GL_LINE_LOOP);
+	{
+		for (int i = 0; i < sideNum; i++)
+		{
+			float theta = degreeR + 2.0f * 3.1415926f * i / sideNum;
+			float xl = size * (cosf(theta));
+			float yl = size * (sinf(theta));
+			glVertex2f(x + xl, y + yl);
+			// glColor3f(0.0,1.0,1.0);
+		}
+	}
+	glEnd();
+}
+
+Point xy(double x, double y)
+{
+	Point v(x, y);
+	return v;
 }
