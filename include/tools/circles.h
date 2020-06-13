03e13cf9042ca0dcbb0fb4a59d515d37bc8a41e5
Martin Emil <mar10martinemil@gmail.com>
2020-06-13 03:07:37 +0200

    Added Circle drawable

--- include/tools/circles.h
+++ include/tools/circles.h
@@ -2,39 +2,44 @@
 #define _CIRCLES_H_
 
 #ifdef __APPLE__
-#include<GLUT/glut.h>
+#include <GLUT/glut.h>
 #else
-#include<GL/glut.h>
+#include <GL/glut.h>
 #endif
 
-#include<stdlib.h>
-#include<stdarg.h>
-#include<math.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <math.h>
 
-#include"point.h"
+#include "point.h"
+#include "polygons.h"
+#include "math.h"
 
-class Circle
+class Circle : public Drawable
 {
-    public:
-        Point center;
-        float radius;
-        int r = 255;
-        int g = 255;
-        int b = 255;
+	public:
+		float radius;
+		
+		Circle();
+		Circle(float x, float y, float radius);
+		Circle(Point center, float radius);
+		Circle(const Circle &v);
 
-        Circle();
-        Circle(float x, float y, float radius);
-        Circle(Point center, float radius);
-        Circle(const Circle &v);
+		~Circle() {}
 
-        ~Circle() {}
+		void set_color(Color color);
+		inline void set_color(int r, int g, int b);
 
-        void set_color(int r, int g, int b);
+		void draw();
+		void save();
+		void set_start(Point start, Color color, bool fill);
+		void change(Point end, Color color, bool fill);
+		Drawable *make_new();
 };
 
-#define draw_circle(cx,cy,radius) draw_circle_(cx,cy,radius,false)
-#define draw_circle_solid(cx,cy,radius) draw_circle_(cx,cy,radius,true)
+#define draw_circle(cx, cy, radius) draw_circle_(cx, cy, radius, false)
+#define draw_circle_solid(cx, cy, radius) draw_circle_(cx, cy, radius, true)
 
-void draw_circle_(float cx,float cy,float radius,bool isSolid);
+void draw_circle_(float cx, float cy, float radius, bool isSolid);
 
 #endif
--- src/tools/circles.cpp
+++ src/tools/circles.cpp
@@ -1,48 +1,101 @@
-#include"circles.h"
+#include "circles.h"
 
 Circle::Circle()
 {
-    this->radius = -1;
+	radius = -1;
+	saved = false;
+	started = false;
 }
 
 Circle::Circle(float x, float y, float radius)
 {
-    this->center = Point(x,y);
-    this->radius = radius;
+	this->start_point = Point(x, y);
+	this->radius = radius;
 }
 
 Circle::Circle(Point center, float radius)
 {
-    this->center = center;
-    this->radius = radius;
+	this->start_point = center;
+	this->radius = radius;
 }
 
 Circle::Circle(const Circle &v)
 {
-    this->center = v.center;
-    this->radius = v.radius;
-    this->r = v.r;
-    this->g = v.g;
-    this->b = v.b;
+	this->start_point = v.start_point;
+	this->radius = v.radius;
+	this->shape_color = v.shape_color;
 }
 
 void Circle::set_color(int r, int g, int b)
 {
-    this->r = r;
-    this->g = g;
-    this->b = b;
+	set_color(Color(r, g, b));
 }
 
-void draw_circle_(float cx,float cy,float radius,bool isSolid)
+void Circle::set_color(Color color)
 {
-    if (isSolid) glBegin(GL_POLYGON);
-    else glBegin(GL_LINE_LOOP); 
-    {
-        for (int i = 0; i < 100; i++){ 
-            float theta = 2.0f * 3.1415926f * i / 100;
-            float x = radius*cosf(theta);
-            float y = radius*sinf(theta);
-            glVertex2f(x+cx,y+cy);
-        } 
-    } glEnd();
+	shape_color = color;
+}
+
+void Circle::draw()
+{
+	shape_color.color();
+	glBegin(filled ? GL_POLYGON : GL_LINE_LOOP);
+	{
+		for (int i = 0; i < 100; i++)
+		{
+			float theta = 2.0f * 3.1415926f * i / 100;
+			float x = radius * cosf(theta);
+			float y = radius * sinf(theta);
+			glVertex2f(x + start_point.x, y + start_point.y);
+		}
+	}
+	glEnd();
+}
+
+void Circle::save()
+{
+	saved = true;
+}
+
+void Circle::set_start(Point start, Color color, bool fill)
+{
+	started = true;
+	shape_color = color;
+	start_point = start;
+}
+
+void Circle::change(Point end, Color color, bool fill)
+{
+	if (!saved & started)
+	{
+		shape_color = color;
+		filled = fill;
+		int x_diff = end.x - start_point.x;
+		int y_diff = end.y - start_point.y;
+		// hard codded canvas dimensions sorry!
+		int tmp_radius = sqrt(x_diff * x_diff + y_diff * y_diff);
+		if (start_point.y + tmp_radius + 75 < 500 && start_point.y - tmp_radius - 10 > 0 &&
+			 start_point.x - tmp_radius - 10 > 0 && start_point.x + tmp_radius + 10 < 500)
+			radius = tmp_radius;
+	}
+}
+
+Drawable *Circle::make_new()
+{
+	return new Circle();
+}
+
+void draw_circle_(float cx, float cy, float radius, bool isSolid)
+{
+	glBegin(isSolid ? GL_POLYGON : GL_LINE_LOOP);
+	{
+		for (int i = 0; i < 100; i++)
+		{
+			float theta = 2.0f * 3.1415926f * i / 100;
+			float x = radius * cosf(theta);
+			float y = radius * sinf(theta);
+			glVertex2f(x + cx, y + cy);
+		}
+	}
+	glEnd();
 }
